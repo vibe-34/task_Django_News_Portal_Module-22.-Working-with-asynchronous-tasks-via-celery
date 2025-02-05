c989963b5de2c1a41009eaa3760810bf651149cb
vibe-34 <vibe.34@gmail.com>
2025-02-05 19:33:05 +0300

    Реализована рассылка подписчикам, через task

--- project/new_portal/signals.py
+++ project/new_portal/signals.py
@@ -1,36 +1,12 @@
-from django.contrib.auth.models import User
-from django.core.mail import EmailMultiAlternatives
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 
 from .models import PostCategory
-from .tasks import *
-from django.conf import settings
+from .tasks import with_every_new_post
 
 
-@receiver(m2m_changed, sender=PostCategory)                      # instance будет по модели Post
+@receiver(m2m_changed, sender=PostCategory)                    # instance будет по модели Post
 def post_created(sender, instance, action, **kwargs):
-    """
-    Автоматическая отправка уведомлений пользователям по e-mail
-    о создании новых постов в категориях, на которые они подписаны.
-    """
+    if action == 'post_add':                                   # Проверяем, что действие - добавление поста
+        with_every_new_post.delay(instance.pk)                 # вызываем нашу таску и передаем ей необходимые аргументы
 
-    if action == 'post_add':                                      # Проверяем, что действие - добавление поста
-        categories = instance.categories.all()                    # Получаем все категории, связанные с созданным постом
-        emails = set()                                            # Используем set для уникальности email-адресов
-
-        # Получаем email-адреса пользователей, подписанных на категорию созданного поста
-        for category in categories:
-            category_emails = User.objects.filter(subscriptions__category=category).values_list('email', flat=True)
-            emails.update(category_emails)                        # Добавляем email-адреса в set
-
-            # Создание содержимого письма
-            if emails:
-
-                # вызываем нашу таску и передаем ей необходимые аргументы
-                with_every_new_post.delay(instance.categories.all(),
-                                          instance.preview(),
-                                          instance.title,
-                                          emails,
-                                          instance.get_absolute_url(),
-                                          )
--- project/new_portal/tasks.py
+++ project/new_portal/tasks.py
@@ -1,28 +1,36 @@
-from celery import shared_task
-# from datetime import datetime, timedelta
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.core.mail import EmailMultiAlternatives
-# from django.template.loader import render_to_string
-# from unicodedata import category
-# from news.models import Category, Subscription
+
+from celery import shared_task
+from new_portal.models import Post
 
 
 @shared_task
-def with_every_new_post(category, preview, title, emails, get_absolute_url):
+def with_every_new_post(pk):
     """Вызывается в сигнале, при создании новой публикации и выполняет рассылку всем подписчикам категории."""
 
-    subject = f'Новая запись в категории {category}'
+    post = Post.objects.get(pk=pk)
+    categories = post.categories.all()                            # Получаем все категории, связанные с созданным постом
+    emails = set()                                                # Используем set для уникальности email-адресов
+
+    # Получаем email-адреса пользователей, подписанных на категорию созданного поста
+    for category in categories:
+        category_emails = User.objects.filter(subscriptions__category=category).values_list('email', flat=True)
+        emails.update(category_emails)                            # Добавляем email-адреса в set
+
+    subject = f'Новая запись в категории {" ,".join(cat.name for cat in post.categories.all())}'
 
     text_content = (
-        f'Название: {title}\n'
-        f'Анонс: {preview}\n\n'
-        f'Ссылка на публикацию: {settings.SITE_URL}{get_absolute_url}'
+        f'Название: {post.title}\n'
+        f'Анонс: {post.preview()}\n\n'
+        f'Ссылка на публикацию: {settings.SITE_URL}{post.get_absolute_url()}'
     )
 
     html_content = (
-        f'Название: {title}<br>'
-        f'Анонс: {preview}<br><br>'
-        f'<a href="{settings.SITE_URL}{get_absolute_url}">'
+        f'Название: {post.title}<br>'
+        f'Анонс: {post.preview()}<br><br>'
+        f'<a href="{settings.SITE_URL}{post.get_absolute_url()}">'
         f'Ссылка на публикацию</a>'
     )
 
